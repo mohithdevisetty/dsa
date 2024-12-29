29d03f5cc407ff71b37f8b05a0d1bf36a4613e75
Mohith Devisetty <127292615+mohithdevisetty@users.noreply.github.com>
2024-12-29 18:58:44 +0530

    patterns

---
+++ src/collections/patterns/Main.java
@@ -0,0 +1,68 @@
+package collections.patterns;
+
+public class Main {
+    public static void main(String[] args) {
+        pattern28(5);
+    }
+
+    static void pattern28(int n) {
+        for (int row = 0; row < 2 * n; row++) {
+            int totalColInRow = row > n ? 2 * n - row : row;
+
+            int noOfSpaces = n - totalColInRow;
+            for (int s = 0; s < noOfSpaces; s++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < totalColInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n) {
+        for (int row = 0; row < 2 * n; row++) {
+            int totalColInRow = row > n ? 2 * n - row : row;
+            for (int col = 0; col < totalColInRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern3(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 0; col <= n - row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern1(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
